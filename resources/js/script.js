211ef54bc1d8d460ed4d138a63fae12c37770482
nhungnt <16520899@gm.uit.edu.vn>
2020-07-30 13:26:46 +0700

    sticky home not working

---
+++ resources/js/script.js
@@ -0,0 +1,20 @@
+/*jslint browser: true*/
+/*global $, jQuery*/
+
+$(document).ready(function() { 
+    $('.js--section-features').wayponit(function (direction) { 
+        if (direction == "down") {
+           $('nav').addClass('sticky'); 
+        } else {
+            $('nav').removeClass('sticky'); 
+        }
+    },  {
+         offset: '60px;'
+    });
+    /*
+   var waypoints = $('#handler-first').waypoint(function(direction) {
+      notify(this.element.id + ' hit 25% from top of window') 
+   }, {
+      offset: '25%'
+   })*/
+});
